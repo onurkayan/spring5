db402f8123cb0f1fe6a04e2cf77d2274edc31091
onur <onur@Mkk-Macbook-3-MacBook-Pro.local>
2017-08-04 12:02:46 +0300

    added some logs

--- src/main/java/com/okayan/springrecipe/controllers/IndexController.java
+++ src/main/java/com/okayan/springrecipe/controllers/IndexController.java
@@ -5,6 +5,7 @@ import com.okayan.springrecipe.domain.UnitOfMeasure;
 import com.okayan.springrecipe.repositories.CategoryRepository;
 import com.okayan.springrecipe.repositories.UnitOfMeasureRepository;
 import com.okayan.springrecipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.Optional;
  * Created by onur on 01/08/2017.
  */
 @Controller
+@Slf4j
 public class IndexController {
 
     private final RecipeService recipeService;
@@ -26,6 +28,7 @@ public class IndexController {
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
 
+        log.info("Inside IndexController");
         model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
